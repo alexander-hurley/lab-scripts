10cb932acfbf9cc021d5647a44e2320f9c41639d
Alexander Hurley <ahurleycanada@gmail.com>
2020-03-06 18:39:30 -0500

    Update restrictfiles.sh

--- restrictfiles.sh
+++ restrictfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Simple script to set permissions on files containing a specific string
 echo "Permissions before changes:"
-ls -l /home/AliceAlexHurley/
+ls -l /home/user/
 echo "Files with ''top_secret'' detected:"
-grep -Ril top_secret /home/AliceAlexHurley/* | while read p; do
+grep -Ril top_secret /home/user/* | while read p; do
         echo $p
-        chown AliceAlexHurley $p
+        chown user $p
         chmod 500 $p
 done
 echo "Permissions after changes:"
